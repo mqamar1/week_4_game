c0f61c2b4422fc4cbc6c483c7314465f489b11b0
mqamar1 <sufimehvish@yahoo.com>
2017-12-16 00:36:36 -0500

    update

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -10,32 +10,12 @@ var assignCrystals = [10, 5, 1, 2];
 
 
 $(document).ready(function() {
-  yourNumber = Math.floor((Math.random() * 99) + 1);
-  $("#yourNum").text(yourNumber);
+  startgame();
 
-  //
-  var crys1 = assignCrystals[Math.floor((Math.random() * assignCrystals.length))]
-  console.log(crys1)
-  $("#crys1img").attr("value", crys1);
-
-  var crys2 = assignCrystals[Math.floor((Math.random() * assignCrystals.length))]
-  console.log(crys2)
-  $("#crys2img").attr("value", crys2);
-
-  var crys3 = assignCrystals[Math.floor((Math.random() * assignCrystals.length))]
-  console.log(crys3)
-  $("#crys3img").attr("value", crys3);
-
-  var crys4 = assignCrystals[Math.floor((Math.random() * assignCrystals.length))]
-  console.log(crys4)
-  $("#crys4img").attr("value", crys4);
-
-
+});
 
   for (var i = 1; i < assignCrystals.length + 1; i++) {
     $("img").addClass("crystalImage");
-    //$(".crystalImage").attr("value", crys1)
-
   }
 
   $(document).on("click", ".crystalImage", function() {
@@ -49,28 +29,34 @@ $(document).ready(function() {
       alert("You Win!")
       crystalWins++;
       totalNumber = 0;
-
-        //console.log(reset)
-
-
-
-
-    } else if (totalNumber >= yourNumber) {
-      alert("You lose!!");
-      crystalLoss++;
-
+      startgame();
     }
 
-
-
+    else if (totalNumber >= yourNumber) {
+      alert("You lose!!");
+      crystalLoss++;
+      totalNumber = 0;
+      startgame();
+    }
     $("#currentTotal").text(totalNumber);
     $("#numWins").text(crystalWins);
     $("#numLoss").text(crystalLoss);
-
-
-
-
   });
 
 
-});
+  function startgame(){
+    yourNumber = Math.floor((Math.random() * 99) + 1);
+    $("#yourNum").text(yourNumber);
+
+    var crys1 = assignCrystals[Math.floor((Math.random() * assignCrystals.length))]
+    $("#crys1img").attr("value", crys1);
+
+    var crys2 = assignCrystals[Math.floor((Math.random() * assignCrystals.length))]
+    $("#crys2img").attr("value", crys2);
+
+    var crys3 = assignCrystals[Math.floor((Math.random() * assignCrystals.length))]
+    $("#crys3img").attr("value", crys3);
+
+    var crys4 = assignCrystals[Math.floor((Math.random() * assignCrystals.length))]
+    $("#crys4img").attr("value", crys4);
+  }
